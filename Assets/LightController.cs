b02c372a9aeec25242460afdb0f55d7c6e2d475d
jvolcy <jerry.volcy@gmail.com>
2024-11-23 07:58:53 -0500

    Added light controls

---
+++ Assets/LightController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    [SerializeField] [Range(0f, 1f)] float SkyboxIntensity = 1f;
+    [SerializeField] Light[] HallLights;
+    [SerializeField] Light[] RoomLights;
+    [SerializeField] [Range(0f, 10f)] float HallLightIntensity = 1f;
+    [SerializeField] [Range(0f, 10f)] float RoomLightIntensity = 1f;
+    [SerializeField] bool DisableSkybox = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.X))
+        //{
+            //RenderSettings.skybox = DisableSkybox ? null : SkyboxMaterial;
+            RenderSettings.ambientIntensity = SkyboxIntensity;
+            RenderSettings.reflectionIntensity = SkyboxIntensity;
+            foreach (Light light in HallLights)
+            {
+                light.intensity = HallLightIntensity;
+            }
+
+            foreach (Light light in RoomLights)
+            {
+                light.intensity = RoomLightIntensity;
+            }
+        //}
+    }
+
+
+}
